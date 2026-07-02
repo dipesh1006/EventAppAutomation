b9770a1c53521faced23665deee2220cbd09512b
Dipesh Pal <dipesh.pal@gmail.com>
2026-07-02 20:20:15 +0530

    add retry mechanism and some others configerations

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,10 +4,37 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   reporter: 'html',
-  timeout: 10 * 1000,
-  use: {
-    browserName: 'chromium',
-    headless: false
-  }
+  retries: 2,
+  workers: 1,
+  timeout: 5 * 1000,
+  expect: {
+    timeout: 5000
+  },
+
+  projects: 
+  [
+    {
+      name: "chrome",
+      use: {
+      browserName: 'chromium',
+      headless: false,
+      screenshot: 'only-on-failure',
+      trace: 'retain-on-failure',
+      ignoreHTTPSErrors: true
+      }
+    },
+    {
+      name: "webkit",
+      use: {
+      browserName: 'webkit',
+      headless: false,
+      screenshot: 'only-on-failure',
+      trace: 'retain-on-failure'
+  
+    }
+
+    }
+
+  ]
 
 });

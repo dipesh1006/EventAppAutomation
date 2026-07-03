1d36e7e25a40879bdb4580d649e45cb6a089b0b0
Dipesh Pal <dipesh.pal@gmail.com>
2026-07-03 17:36:08 +0530

    adding enum to manage the api resources

--- tests/APItest.spec.ts
+++ tests/APItest.spec.ts
@@ -4,6 +4,7 @@ import LoginAPI from '../testData/apiRequest/LoginAPI.json';
 import CreateEventAPI from '../testData/apiRequest/CreateEventAPI.json';
 import BookEventAPI from '../testData/apiRequest/BookEventAPI.json';
 import { APIUtils } from '../utils/APIUtils';
+import { APIResource } from '../utils/APIResource';
 
 test(`Event ticket book API test`, {tag: ['@API','@regression']} , async () => {
 
@@ -16,7 +17,7 @@ test(`Event ticket book API test`, {tag: ['@API','@regression']} , async () => {
 
     // Login API
 
-    const token = await apiUtils.generateToken(apiContext,"auth/login",LoginAPI);
+    const token = await apiUtils.generateToken(apiContext,APIResource.LoginAPI,LoginAPI);
     console.log("Here is the Token: "+ token);
 
     // Added Token to the header
@@ -27,7 +28,7 @@ test(`Event ticket book API test`, {tag: ['@API','@regression']} , async () => {
 
     // Event Creation API
 
-    const EventAPIresponsePayload = await apiUtils.callPostAPI(apiContext,"events",CreateEventAPI,header)
+    const EventAPIresponsePayload = await apiUtils.callPostAPI(apiContext,APIResource.EventAPI,CreateEventAPI,header)
     const eventid = await EventAPIresponsePayload.data.id;
     console.log(`Here is the event id ${eventid}`);
    
@@ -35,14 +36,14 @@ test(`Event ticket book API test`, {tag: ['@API','@regression']} , async () => {
 
     BookEventAPI.eventId = eventid;
 
-    const BookEventAPIresponsePayload = await apiUtils.callPostAPI(apiContext,"bookings",BookEventAPI,header)
+    const BookEventAPIresponsePayload = await apiUtils.callPostAPI(apiContext,APIResource.BookEventAPI,BookEventAPI,header)
     console.log(await BookEventAPIresponsePayload.message);
 
     // Delete that Event
 
     const pathParams:any[] = [eventid]; // create an array of all path parameters
 
-    const deleteEventResponse = await apiUtils.callDeleteAPI(apiContext,"events",header,pathParams);
+    const deleteEventResponse = await apiUtils.callDeleteAPI(apiContext,APIResource.EventAPI,header,pathParams);
     console.log(await deleteEventResponse.message);
 
 
---
+++ utils/APIResource.ts
@@ -0,0 +1,8 @@
+export enum APIResource {
+
+    LoginAPI = "auth/login",
+    EventAPI = "events",
+    BookEventAPI = "bookings",
+
+
+}
